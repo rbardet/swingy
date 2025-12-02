8b7f2e6d5a15624beebec93e76470fa8d8f7edd6
rbardet <robin.bardet.tomczak@gmail.com>
2025-12-02 23:32:37 +0100

    Refactor: change db logic -> everything in 1 table

--- srcs/en/swingy/db/schema.sql
+++ srcs/en/swingy/db/schema.sql
@@ -1,40 +1,15 @@
-CREATE TABLE Stats (
-	"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"attack" INTEGER NOT NULL,
-	"defense" INTEGER NOT NULL,
-	"hit_points" INTEGER NOT NULL
-);
-
-CREATE TABLE Weapon (
-	"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"name" TEXT NOT NULL,
-	"attack" INTEGER NOT NULL
-);
-
-CREATE TABLE Armor (
-	"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"name" TEXT NOT NULL,
-	"defense" INTEGER NOT NULL
-);
-
-CREATE TABLE Helm (
-	"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"name" TEXT NOT NULL,
-	"hit_points" INTEGER NOT NULL 
-);
-
 CREATE TABLE Hero (
 	"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	"name" TEXT NOT NULL,
 	"lv" INTEGER NOT NULL,
 	"xp" INTEGER NOT NULL,
-	"stats_id" INTEGER NOT NULL,
-	"weapon_id" INTEGER,
-	"armor_id" INTEGER,
-	"helm_id" INTEGER,
-
-	FOREIGN KEY("stats_id") REFERENCES Stats("id"),
-	FOREIGN KEY("weapon_id") REFERENCES Weapon("id"),
-	FOREIGN KEY("armor_id") REFERENCES Armor("id"),
-	FOREIGN KEY("helm_id") REFERENCES Helm("id")
+	"attack" INTEGER NOT NULL,
+	"defense" INTEGER NOT NULL,
+	"hit_points" INTEGER NOT NULL,
+	"w_name" TEXT NOT NULL,
+	"w_attack" INTEGER NOT NULL,
+	"a_name" TEXT NOT NULL,
+	"a_defense" INTEGER NOT NULL,
+	"h_name" TEXT NOT NULL,
+	"h_hp" INTEGER NOT NULL
 );
