84a5d9817a20c3e14bd805e0560819458f913130
rbardet <robin.bardet.tomczak@gmail.com>
2025-12-05 00:10:05 +0100

    Chore: change game var in main

--- srcs/en/swingy/Main.java
+++ srcs/en/swingy/Main.java
@@ -5,8 +5,6 @@ import java.sql.SQLException;
 import en.swingy.game.Game;
 
 public class Main {
-	private static Game game = new Game();
-
 	public static void usage(String args[]) {
 		if (args.length != 1) {
 			System.out.println("Usage: java -jar swingy.jar [console/gui]");
@@ -15,6 +13,7 @@ public class Main {
 	}
 
 	public static void main(String args[]) throws SQLException {
+		Game game = new Game();
 		if (args[0].equals("gui")) {
 			game.setGui(true);
 		} else if (args[0].equals("console")) {
