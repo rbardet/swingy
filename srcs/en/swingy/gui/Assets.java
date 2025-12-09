6b7eed463a5800550e29def88941df4c0b5f16e9
rbardet <robin.bardet.tomczak@gmail.com>
2025-12-09 03:03:00 +0000

    Feat: add Player inv sprite loc

--- srcs/en/swingy/gui/Assets.java
+++ srcs/en/swingy/gui/Assets.java
@@ -6,12 +6,13 @@ public class Assets {
 	public static final String CLASS_ICON_PATH = ASSETS_PATH + "classes-icon/";
 	public static final String MENU_BUTTON =  ASSETS_PATH + "menu_button.png";
 	public static final String MENU_BUTTON_HOVER = ASSETS_PATH + "menu_button_hover.png";
-	public static final String MENU_FONT = ASSETS_PATH + "menu_font.png";
+	public static final String MENU_BG = ASSETS_PATH + "menu_bg.png";
 	public static final String MENU_QUIT = ASSETS_PATH + "menu_quit.png";
 	public static final String MENU_QUIT_HOVER = ASSETS_PATH + "menu_quit_hover.png";
 	public static final String USERNAME = ASSETS_PATH + "username.png";
 	public static final String USERNAME_FIELD = ASSETS_PATH + "username_field.png";
 	public static final String BANNER = ASSETS_PATH + "banner.png";
+	public static final String PLAYER_INV = ASSETS_PATH + "player_inv.png";
 
 	public static final String MONK_BG = CLASS_BG_PATH + "md_monk_background.png";
 	public static final String BARBARIAN_BG = CLASS_BG_PATH + "md_barbarian_background.png";
