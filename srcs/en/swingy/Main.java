137a35410fadc1b33569edcd090c2fce17b561b5
rbardet <robin.bardet.tomczak@gmail.com>
2026-01-07 23:54:42 +0100

    Fix(crash): lil bug with gamemode

--- srcs/en/swingy/Main.java
+++ srcs/en/swingy/Main.java
@@ -4,10 +4,8 @@ import en.swingy.game.Game;
 
 public class Main {
 	public static void usage(String args[]) {
-		if (args.length != 1) {
-			System.out.println("Usage: java -jar swingy.jar [console/gui]");
-			System.exit(1);;
-		}
+		System.out.println("Usage: java -jar swingy.jar [console/gui]");
+		System.exit(1);;
 	}
 
 	public static void main(String args[]) throws Exception {
